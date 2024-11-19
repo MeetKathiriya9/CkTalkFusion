02987223a3310a9236ac356712863ac5331c100e
MeetKathiriya9 <kohlinbumrah@gmail.com>
2024-11-19 13:28:10 +0530

    update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -10,10 +10,10 @@
 // };
 
 
-const apiUrl = "https://cktalkfusion-backend.onrender.com/";
+const apiUrl = "https://cktalkfusion-backend.onrender.com";
 // Function to make API calls (optional wrapper)
 export const fetchAPI = async (endpoint, options = {}) => {
-  const response = await fetch(`${apiUrl}${endpoint}`, options);
+  const response = await fetch(`${apiUrl}/${endpoint}`, options);
 
   if (!response.ok) {
     throw new Error(`API call failed: ${response.statusText}`);
